77860cde6ffff78c330c52b925d54942d590f00a
java13070713 <java13070713@ldap.com>
2022-08-13 17:32:12 +0530

    node modules updated

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./component/NavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
